e0b1fd060213a73cde1b8353c1cbed9f2f0e01cb
Mitresh <mitreshdabhi167@gmail.com>
2023-04-24 23:49:26 +0530

    Intial commit

---
+++ flight_data.py
@@ -0,0 +1,10 @@
+
+class FlightData:
+    def __init__(self, origin_city, destination_city, going_date, origin_airport, destination_airport, return_date, price):
+        self.origin_city = origin_city 
+        self.destination_city = destination_city
+        self.going_date = going_date
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.return_date = return_date
+        self.price = price
